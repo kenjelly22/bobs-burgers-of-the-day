09f9ee0548c716742d743fa8b3a6543cd0bc4795
Jen Kelly <jen.kelly@powerhrg.com>
2025-06-30 16:26:28 -0400

    App loads

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +14,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello!
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
