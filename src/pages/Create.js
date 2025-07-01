534b6a0ab62c1a8ee04e032b41cb45e8bd0f7945
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-01 19:55:02 -0400

    Create form for new burger

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,4 +1,38 @@
-function Create() {
-  return <h1>Create Page</h1>
+import React, {useState} from "react"
+
+const Create = () => {
+  const [newBurger, setNewBurger] = useState({
+    name: "",
+    price: "",
+  })
+  return (
+    <div className="container">
+      <form className="add-burger-form">
+        <h3>Create a Burger</h3>
+        <input
+          type="text"
+          name="name"
+          value={newBurger.name}
+          placeholder="burger name..."
+          className="input-text"
+        />
+        <br />
+        <input
+          type="text"
+          name="price"
+          value={newBurger.price}
+          placeholder="burger price..."
+          className="input-text"
+        />
+        <br />
+        <input
+          type="submit"
+          name="submit"
+          value="Create New Toy"
+          className="submit"
+        />
+      </form>
+    </div>
+  )
 }
 export default Create
