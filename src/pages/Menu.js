5de607630dd89f986e8ca5e050e459750bcad555
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-01 17:56:35 -0400

    Fetch burgers on page load. Map through results and pass to MenuItem for rendering

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,4 +1,27 @@
+import {useEffect, useState} from "react"
+import MenuItem from "../components/MenuItem"
+
 function Menu() {
-  return <h1>Menu Page</h1>
+  const [burgers, setBurgers] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/burgers")
+      .then((r) => r.json())
+      .then((burgers) => {
+        setBurgers(burgers)
+      })
+  }, [])
+  return (
+    <>
+      <h1>Menu Page</h1>
+      <div>
+        <ul>
+          {burgers.map((burger) => (
+            <MenuItem key={burger.id} burger={burger} />
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
 export default Menu
