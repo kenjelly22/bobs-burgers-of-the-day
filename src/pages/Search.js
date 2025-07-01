91907364cbc6a93cc78d2f82c5e42fef722315d4
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-01 15:26:07 -0400

    Run fetch onClick and return results that match the searchTerm

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,23 +2,37 @@ import React, {useState} from "react"
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("")
+  const [allBurgers, setAllBurgers] = useState([])
 
   const handleChange = ({target: {value}}) => {
     setSearchTerm(value)
   }
 
+  const handleSearch = () => {
+    fetch("https://bobsburgers-api.herokuapp.com/burgerOfTheDay/")
+      .then((r) => r.json())
+      .then((burgers) => {
+        setAllBurgers(burgers)
+        const filteredBurgers = burgers.filter((burger) =>
+          burger.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        setAllBurgers(filteredBurgers)
+      })
+  }
+
   return (
     <div className="searchbar">
+      <h1>Find a Burger</h1>
       <div>
         <input
           className="ui search"
           type="text"
           id="search"
           value={searchTerm}
-          placeholder="Search for a Burger"
+          placeholder="Search for new Burger"
           onChange={handleChange}
-        ></input>
-        <i className="search icon" />
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
     </div>
   )
