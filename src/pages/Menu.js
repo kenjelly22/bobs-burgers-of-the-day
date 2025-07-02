2a22715779c9d8be6ef8ed359cd2291024c1f83c
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-01 22:35:29 -0400

    Add delete button to Menu. Remove menu item from page and db.json

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -11,6 +11,12 @@ function Menu() {
         setBurgers(burgers)
       })
   }, [])
+
+  const handleDelete = (deletedBurger) => {
+    const updatedMenu = burgers.filter((burger) => burger.id !== deletedBurger)
+    setBurgers(updatedMenu)
+  }
+
   return (
     <>
       <h1>Welcome to Bob's Burgers</h1>
@@ -18,7 +24,7 @@ function Menu() {
         <h2>Menu</h2>
         <ul>
           {burgers.map((burger) => (
-            <MenuItem key={burger.id} burger={burger} />
+            <MenuItem key={burger.id} burger={burger} onDelete={handleDelete} />
           ))}
         </ul>
       </div>
