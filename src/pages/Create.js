04dec784c390cc39399ed6fddc83b63e0c6c4313
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-01 22:11:53 -0400

    Add submit button, post new burger to db.json and display on Menu

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react"
 
-const Create = () => {
+const Create = ({setBurgerList}) => {
   const [newBurger, setNewBurger] = useState({
     name: "",
     price: "",
@@ -14,9 +14,29 @@ const Create = () => {
     setNewBurger(updatedBurger)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    fetch("http://localhost:3001/burgers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: newBurger.name,
+        price: `$` + newBurger.price,
+      }),
+    })
+      .then((r) => r.json())
+      .then((createdBurger) => {
+        setBurgerList((prev) => [...prev, createdBurger])
+        setNewBurger({name: "", price: ""})
+      })
+  }
+
   return (
     <div className="container">
-      <form className="add-burger-form">
+      <form className="add-burger-form" onSubmit={handleSubmit}>
         <h3>Create a Burger</h3>
         <input
           type="text"
